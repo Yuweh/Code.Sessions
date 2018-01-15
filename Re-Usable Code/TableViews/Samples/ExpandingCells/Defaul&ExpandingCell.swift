f1ce94c573f4c23bc8a5c32be2523e522d904b83
Francis <Yuweh@users.noreply.github.com>
2018-01-15 12:31:50 +0800

    updated

---
+++ Re-Usable Code/TableViews/Samples/ExpandingCells/Defaul&ExpandingCell.swift
@@ -0,0 +1,30 @@
+
+
+import Foundation
+import UIKit
+
+class FAQDefaultCell : UITableViewCell {
+    
+    @IBOutlet weak var QuestionLabel: UILabel!
+    
+}
+
+import Foundation
+import UIKit
+
+class FAQExpansionCell : UITableViewCell {
+    
+    @IBOutlet weak var AnswerLabel: UITextView!
+    
+}
+
+
+import Foundation
+import UIKit
+
+class FAQExpansionCell : UITableViewCell {
+    
+    @IBOutlet weak var AnswerLabel: UITextView!
+    
+}
+
