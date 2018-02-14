0a6dec02729589e2e8213a94a6829c7b463b7698
Francis <Yuweh@users.noreply.github.com>
2018-02-14 12:55:23 +0800

    updated

--- Re-Usable Code/TouchID/Sample/ErrorHandlingSAMPLE.swift
+++ Re-Usable Code/TouchID/Sample/ErrorHandlingSAMPLE.swift
@@ -1,5 +1,65 @@
+//RayWenderlich
 
-#updated for iOS 11 :D
+        let message: String
+
+        switch evaluateError {
+        case LAError.authenticationFailed?:
+          message = "There was a problem verifying your identity."
+        case LAError.userCancel?:
+          message = "You pressed cancel."
+        case LAError.userFallback?:
+          message = "You pressed password."
+        case LAError.biometryNotAvailable?:
+          message = "Face ID/Touch ID is not available."
+        case LAError.biometryNotEnrolled?:
+          message = "Face ID/Touch ID is not set up."
+        case LAError.biometryLockout?:
+          message = "Face ID/Touch ID is locked."
+        default:
+          message = "Face ID/Touch ID may not be configured"
+        }
+        completion(message)  
+
+
+//Error Messages:
+#updated for iOS 11 
+
+                    let message: String
+                    
+                    switch evaluateError {
+                    case LAError.appCancel?:
+                        message = "Authentication was cancelled by application"
+                        
+                    case LAError.authenticationFailed?:
+                        message = "The user failed to provide valid credentials"
+                        
+                    case LAError.invalidContext?:
+                        message = "The context is invalid"
+                        
+                    case LAError.passcodeNotSet?:
+                        message = "Passcode is not set on the device"
+                        
+                    case LAError.systemCancel?:
+                        message = "Authentication was cancelled by the system"
+                        
+                    case LAError.touchIDLockout?:
+                        message = "Too many failed attempts."
+                        
+                    case LAError.touchIDNotAvailable?:
+                        message = "TouchID is not available on the device"
+                        
+                    case LAError.userCancel?:
+                        message = "The user did cancel"
+                        
+                    case LAError.userFallback?:
+                        message = "The user chose to use the fallback"
+                    default:
+                        message = "Face ID/Touch ID may not be configured"
+
+
+
+
+#updated for iOS 11 with errorCode
 
     func errorMessageForLAErrorCode(errorCode:Int) -> String{
         
