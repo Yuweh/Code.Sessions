8285b04de5fedc4bc310db67ab9aa553b1b0fbda
Francis <Yuweh@users.noreply.github.com>
2018-03-20 15:56:54 +0800

    updated

--- Security/CrytoSWIFT/SAMPLE.swift
+++ Security/CrytoSWIFT/SAMPLE.swift
@@ -2,8 +2,8 @@ import Cryptoswift
 
     func encryptUserData(key: String, iv: String, userData: String) -> String {
         do {
-            let data = userData.data(using: String.Encoding.utf8, allowLossyConversion: true)
-            let encrypted = try! AES(key: key.bytes, blockMode: .CBC(iv: iv.bytes), padding: .pkcs7).encrypt([UInt8](data!))
+            let data = userData.data(using: .utf8)!
+            let encrypted = try! AES(key: key.bytes, blockMode: .CBC(iv: iv.bytes), padding: .pkcs7).encrypt([UInt8](data))
             let encryptedData = Data(encrypted)
             return encryptedData.base64EncodedString()
         }
@@ -13,13 +13,8 @@ import Cryptoswift
     }
     
     func decryptUserData(key: String, iv: String, userData: String) -> String {
-        do {
-            let data = userData.data(using: String.Encoding.utf8, allowLossyConversion: true)
-            let decrypted = try! AES(key: key.bytes, blockMode: .CBC(iv: iv.bytes), padding: .pkcs7).decrypt([UInt8](data!))
-            let decryptedData = Data(decrypted)
-            return String(bytes: decryptedData.bytes, encoding: .utf8) ?? "Could not decrypt"
-        }
-        catch {
-            print("error")
-        }
+        let data = Data(base64Encoded: userData)!
+        let decrypted = try! AES(key: key.bytes, blockMode: .CBC(iv: iv.bytes), padding: .pkcs7).decrypt([UInt8](data))
+        let decryptedData = Data(decrypted)
+        return String(bytes: decryptedData.bytes, encoding: .utf8) ?? "Could not decrypt"
     }
