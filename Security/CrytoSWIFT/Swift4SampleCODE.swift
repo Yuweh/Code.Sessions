44c27379560f2fada55adeb018a142d83695ad09
Francis <Yuweh@users.noreply.github.com>
2018-04-11 11:34:43 +0800

    updated

--- Security/CrytoSWIFT/Swift4SampleCODE.swift
+++ Security/CrytoSWIFT/Swift4SampleCODE.swift
@@ -1,5 +1,23 @@
-    //TESTCODE
+    //TESTCODE -> solves deprecated String issue ("Value of type of String has no member 'bytes')
+
+    // previous code ->
+        func encryptUserData(key: String, iv: String, userData: String) -> String {
+        let data = userData.data(using: .utf8)!
+        let encrypted = try! AES(key: key.bytes, blockMode: .CBC(iv: iv.bytes), padding: .pkcs7).encrypt([UInt8](data))
+        let encryptedData = Data(encrypted)
+        return encryptedData.base64EncodedString()
+    }
     
+    func decryptUserData(key: String, iv: String, userData: String) -> String {
+        let data = Data(base64Encoded: userData)!
+        let decrypted = try! AES(key: key.bytes, blockMode: .CBC(iv: iv.bytes), padding: .pkcs7).decrypt([UInt8](data))
+        let decryptedData = Data(decrypted)
+        return String(bytes: decryptedData.bytes, encoding: .utf8) ?? "Could not decrypt"
+    }
+
+    
+    //TESTCODE Implemented - to be improved with guard or error handling :D
+
     //DECRYPT (Simple version / no padding and blockmode specified)
     func decryptUserData(key: String, iv: String, userData: String) -> String {
         let data = Data(base64Encoded: userData)!
