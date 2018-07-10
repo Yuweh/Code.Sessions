8113453722a44209023ddd59876e6a0bfde3bee6
Jay <Yuweh@users.noreply.github.com>
2018-07-10 16:52:32 +0800

    (っ˘▽˘)っ updated ⊂(◕.◕⊂)

--- Re-Usable Code/Temperature/Computation.swift
+++ Re-Usable Code/Temperature/Computation.swift
@@ -63,18 +63,93 @@
 
 
 
-// MARK: THI Setter
+    // MARK: getTHIText
+    
+    static func getTHIText(preferences: UserDefaults, data: String) -> String {
+        
+        if let rawData = Double(data.trimmingCharacters(in: TktConstants.CharSet.climateDigits.inverted)) {
+            if (rawData >= 15){ // Check if IM read returns zero value
+                return "\(String(format: "%.1f", rawData))\(TktConstants.Units.THI)"
+            }
+        }
+        return ""
+    }
 
-forKey: TktConstants.Key.UnitPreference
+
+    //MARK: GetTHITextColor
+    static func getTHITextColor(view: String, preferences: UserDefaults, thiValue: String, address: String) -> UIColor {
+        
+        if let thiValue = Double(thiValue.trimmingCharacters(in: TktConstants.CharSet.climateDigits.inverted)) {
+            if (view == TktConstants.CallingView.SensorListViewController) {
+                return (thiValue >= TktConstants.Default.THI_THRESHOLD_MAX) ? TktConstants.Color.Basic.White : TktConstants.Color.Basic.White
+            }
+            if thiValue >= TktConstants.Default.THI_THRESHOLD_MAX {
+                return TktConstants.Color.AirComfort.WarningTextColor
+            } else if ((thiValue >= TktConstants.Default.THI_MODERATE_MIN) && (thiValue <= TktConstants.Default.THI_MODERATE_MAX)) {
+                return TktConstants.Color.Basic.Orange
+            } else if thiValue <= TktConstants.Default.THI_THRESHOLD_MIN {
+                return TktConstants.Color.Basic.White
+            }
+        }
+        
+        return TktConstants.Color.Basic.Clear
+    }
+
+        // MARK: THI Get IconImage
+        case TktConstants.Key.attrTHI:
+            if let thi = Double(value.trimmingCharacters(in: TktConstants.CharSet.climateDigits.inverted)) {
+                if (view == TktConstants.CallingView.SensorListViewController) {
+                    return (thi >= TktConstants.Default.THI_THRESHOLD_MAX) ? UIImage(named: "thi_Red")! : UIImage(named: "thi_White")!
+                }
+                if thi >= TktConstants.Default.THI_THRESHOLD_MAX {
+                    return UIImage(named: "thi_Red")!
+                } else if thi >= TktConstants.Default.THI_MODERATE_MIN {
+                    return UIImage(named: "thi_Orange")!
+                } else if thi <= TktConstants.Default.THI_THRESHOLD_MIN {
+                    return UIImage(named: "thi_White")!
+                } else if thi <= 15.0 {
+                    return UIImage(named: "thi_Red")!
+                }
+            }
+
+        //THI InfoPage Values
+        static let THI_THRESHOLD_MIN: Double = 67.0 //67 below = White
+        static let THI_THRESHOLD_MAX: Double = 75.0 //75 = Red
+        static let THI_MODERATE_MIN: Double = 68.0  //
+        static let THI_MODERATE_MAX: Double = 74.0
 
 
 
 
+    // MARK: THI @Settings Observer
+    private func setTHIobservers(address: String) {
+        
+        let showTHIKey = "\(address)\(TktConstants.Key.ShowSensorTHI)"
 
+        if ((preferences.object(forKey: showTHIKey) != nil) && (preferences.object(forKey: TktConstants.Key.SettingsTHIVisibility) != nil)) {
+            let settingsNotifier = preferences.integer(forKey: showTHIKey)
+            let debugNotifier = preferences.integer(forKey: TktConstants.Key.SettingsTHIVisibility)
+            if ((settingsNotifier == 1) && (debugNotifier == 1)) {
+                self.thiContainerEnabler(isHidden: false)
+            } else if ((settingsNotifier == 0) && (debugNotifier == 1)) {
+                self.thiContainerEnabler(isHidden: true)
+            } else {
+                self.thiContainerEnabler(isHidden: true)
+            }
+        }
+    }
+    
+    private func thiContainerEnabler(isHidden: Bool) {
+        self.thiStackView.isHidden = isHidden
+        self.defaultStackView.isHidden = !isHidden
+    }
 
-
-
-
+    private func thiContainerEnabler(isHidden: Bool) {
+            self.thiTopStackView.isHidden = isHidden
+            self.thiBottomStackView.isHidden = isHidden
+            self.rightDefaulStackView.isHidden = !isHidden
+            self.leftDefaulStackView.isHidden = !isHidden
+    }
 
 
 
