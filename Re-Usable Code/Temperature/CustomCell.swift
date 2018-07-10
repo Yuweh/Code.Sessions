e090f9f04c4df4aa68e83e48687056c73c69db8c
Jay <Yuweh@users.noreply.github.com>
2018-07-10 17:23:55 +0800

    (っ˘▽˘)っ updated ⊂(◕.◕⊂)

--- Re-Usable Code/Temperature/CustomCell.swift
+++ Re-Usable Code/Temperature/CustomCell.swift
@@ -44,8 +44,51 @@
     }
     
 
-    
-    
+    private func setUpDefaultViewElements(data: SensorListCellData) {
+        
+        self.sensorName.text = data.sensorName
+        self.sensorIcon.setImage(data.sensorIcon, for: .normal)
+        self.sensorIcon.imageView?.contentMode = .scaleAspectFill
+        self.sensorIcon.cornerRadius = 25.0
+        
+        self.sensorTemperatureImage.image = Globals.getIconImage(view: TktConstants.CallingView.SensorListViewController, preferences: self.preferences,
+                                                                 value: data.sensorTemperature!, dataType: TktConstants.Key.attrTemperature, address: data.sensorMacAddress!)
+        self.sensorTemperature.text = Globals.getDataText(preferences: self.preferences, data: data.sensorTemperature!, isTemperature: true)
+        self.sensorTemperature.textColor = Globals.getTemperatureTextColor(view: TktConstants.CallingView.SensorListViewController, preferences: self.preferences, temperature: data.sensorTemperature, address: data.sensorMacAddress!)
+        
+        self.sensorHumidityImage.image = Globals.getIconImage(view: TktConstants.CallingView.SensorListViewController, preferences: self.preferences,
+                                                              value: data.sensorHumidity!, dataType: TktConstants.Key.attrHumidity, address: data.sensorMacAddress!)
+        self.sensorHumidity.text = Globals.getDataText(preferences: self.preferences, data: data.sensorHumidity!, isTemperature: false)
+        self.sensorHumidity.textColor = Globals.getHumidityTextColor(view: TktConstants.CallingView.SensorListViewController, preferences: self.preferences, humidity: data.sensorHumidity!, address: data.sensorMacAddress!)
+        
+        if let lastUpdatedTimestamp = Double(data.sensorLastUpdated) {
+            self.timeStamp.isHidden = (lastUpdatedTimestamp != 0.0) ? false : true
+            self.setReferenceTime(referenceTime: lastUpdatedTimestamp)
+        }
+    }
+        
+        private func setUpTHIViewElements(data: SensorListCellData) {
+        
+        self.sensorNameTHI.text = data.sensorName
+        self.sensorIconTHI.setImage(data.sensorIcon, for: .normal)
+        self.sensorIconTHI.imageView?.contentMode = .scaleAspectFill
+        self.sensorIconTHI.cornerRadius = 25.0
+        
+        self.sensorTemperatureImageTHI.image = Globals.getIconImage(view: TktConstants.CallingView.SensorListViewController, preferences: self.preferences,
+                                                                 value: data.sensorTemperature!, dataType: TktConstants.Key.attrTemperature, address: data.sensorMacAddress!)
+        self.sensorTemperatureTHI.text = Globals.getDataText(preferences: self.preferences, data: data.sensorTemperature!, isTemperature: true)
+        self.sensorTemperatureTHI.textColor = Globals.getTemperatureTextColor(view: TktConstants.CallingView.SensorListViewController, preferences: self.preferences, temperature: data.sensorTemperature, address: data.sensorMacAddress!)
+        
+        self.sensorHumidityImageTHI.image = Globals.getIconImage(view: TktConstants.CallingView.SensorListViewController, preferences: self.preferences,
+                                                              value: data.sensorHumidity!, dataType: TktConstants.Key.attrHumidity, address: data.sensorMacAddress!)
+        self.sensorHumidityTHI.text = Globals.getDataText(preferences: self.preferences, data: data.sensorHumidity!, isTemperature: false)
+        self.sensorHumidityTHI.textColor = Globals.getHumidityTextColor(view: TktConstants.CallingView.SensorListViewController, preferences: self.preferences, humidity: data.sensorHumidity!, address: data.sensorMacAddress!)
+        
+        if let lastUpdatedTimestamp = Double(data.sensorLastUpdated) {
+            self.timeStampTHI.isHidden = (lastUpdatedTimestamp != 0.0) ? false : true
+            self.setReferenceTime(referenceTime: lastUpdatedTimestamp)
+        }
+    }
     
     
     
