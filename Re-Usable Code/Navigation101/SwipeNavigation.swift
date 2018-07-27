c96322e71e38756065d8f88cf11b555e2977bcaf
Jay <Yuweh@users.noreply.github.com>
2018-07-27 10:42:07 +0800

    Update SwipeNavigation.swift

--- Re-Usable Code/Navigation101/SwipeNavigation.swift
+++ Re-Usable Code/Navigation101/SwipeNavigation.swift
@@ -1,4 +1,22 @@
 
+//
+        let swipeGesture:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(showLuggageMenu(_:)))
+        swipeGesture.direction = .right
+        swipeGesture.numberOfTouchesRequired = 1
+        self.view.addGestureRecognizer(swipeGesture)
+
+    @objc func showLuggageMenu(_ sender: Any) {
+        let newViewController = self.storyboard?.instantiateViewController(withIdentifier: "LuggageMenuViewController")
+        self.transitionLayer.isHidden = false
+        let transition = CATransition()
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromLeft
+        self.transitionLayer.window?.layer.add(transition, forKey: kCATransition)
+        self.present(newViewController!, animated: false, completion: nil)
+        Logger.log("SWIPE WORKING")
+    }
+
+
 class FirstSceneViewController: UIViewController {
 
     override func viewDidLoad() {
