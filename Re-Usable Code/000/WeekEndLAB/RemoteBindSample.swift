63ce27a6289acc2fb2ffcd1a232bf54ff5100e4d
Jay <Yuweh@users.noreply.github.com>
2018-09-04 11:01:20 +0800

    Create RemoteBindSample.swift

---
+++ Re-Usable Code/000/WeekEndLAB/RemoteBindSample.swift
@@ -0,0 +1,15 @@
+
+
+    
+//    fileprivate func getUnselectedDeviceIdsPerChannel() -> [Int: [String]] {
+//        var unselectedDeviceIds: [Int: [String]] = [:]
+//        for i in 1...self.mChannelCount {
+//
+//            if !getUnselectedMacAddress(channel: 1).isEmpty {
+//                unselectedDeviceIds[i] = getUnselectedMacAddress(channel: i)
+//            }
+//        }
+//
+//        return unselectedDeviceIds
+//    }
+
