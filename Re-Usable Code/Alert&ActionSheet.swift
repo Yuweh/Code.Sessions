c05518091cae131546d217f2fff2b9478a2e5362
Jay <Yuweh@users.noreply.github.com>
2018-12-18 19:13:52 +0800

    (っ˘▽˘)っ updated ⊂(◕.◕⊂)

--- Re-Usable Code/Alert&ActionSheet.swift
+++ Re-Usable Code/Alert&ActionSheet.swift
@@ -1,5 +1,25 @@
 
-    //MARK: ActionSheet SAMPLE
+//MARK: In APPDELEGATE (regardless of VC)    
+
+    @objc func localModeAlertBox() {
+        print("*** localModeAlertBox SHOWN for LocalGatewayManager")
+        let alert = UIAlertController(title: "", message:"You have been disconnected from our server. \n\nDo you to proceed to Local Mode?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "config_common_js_no".localized, style: UIAlertActionStyle.cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "config_common_js_yes".localized, style: UIAlertActionStyle.destructive, handler: nil))
+        
+        // show alert
+        let alertWindow = UIWindow(frame: UIScreen.main.bounds)
+        alertWindow.rootViewController = UIViewController()
+        alertWindow.windowLevel = UIWindowLevelAlert + 1;
+        alertWindow.makeKeyAndVisible()
+        alertWindow.rootViewController?.present(alert, animated: true, completion: nil)
+    }
+
+
+
+//MARK: ActionSheet SAMPLE
     
     func bottomActionSheetPressed() {
         let alert = UIAlertController(title: nil, message: "Get an image of your check", preferredStyle: .actionSheet)
