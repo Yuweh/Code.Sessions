0a7586fe629f4478acebcb5014da8cdc83c1f57d
Jay <Yuweh@users.noreply.github.com>
2020-09-20 22:39:09 +0800

    complete

---
+++ Re-Usable Code/OnBoarding/ViewController.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class IntroViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if Core.shared.isNewUser() {
+            //show boarding
+            let vc = storyboard?.instantiateViewController(identifier: "welcome") as! WelcomeViewController
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true, completion: nil)
+        }
+    }
+}
+
+class Core {
+    
+    static let shared = Core()
+    
+    func isNewUser() -> Bool {
+        return !UserDefaults.standard.bool(forKey: "isNewUser")
+    }
+    
+    func setIsNotNewUser() {
+        UserDefaults.standard.set(true, forKey: "isNewUser")
+    }
+}
